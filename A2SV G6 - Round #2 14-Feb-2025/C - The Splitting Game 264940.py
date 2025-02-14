c8767bdf24cb913b72e2eedf1ae94eaeba237f48
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-02-14 09:45:32 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 14-Feb-2025/C - The Splitting Game 264940.py
@@ -0,0 +1,23 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from typing import Counter
+
+
+def dis_char ():
+    x = int(input())
+    str = input()
+    Max=0
+    count = Counter(str)
+    count1 = Counter()
+    for i in range(x):
+        count[str[i]] -= 1
+        count1[str[i]] += 1
+        if count[str[i]] == 0:
+            del count[str[i]]
+        Max = max(Max,len(count)+len(count1))
+    print (Max)
+
+x = int(input())
+for _ in range(x):
+    dis_char()
+    
