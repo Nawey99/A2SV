e5258a25e38b371ff2cccf1786d2c49f7f8616bd
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-02-18 15:26:35 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 18-Feb-2025/Alternating Subsequence 271685.py
@@ -0,0 +1,27 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+
+def solution():
+    n = int(input())
+    subs = list(map(int,input().split()))
+    l = 0
+    bmax = 0
+    while l < n:
+        if subs[l] >= 0:
+            Max = 0
+            while l<n and subs[l] > 0 :
+                Max = max(Max, subs[l])
+                l += 1
+            bmax += Max
+        else:
+            Max = subs[l]
+            while l<n and subs[l] < 0:
+                Max = max(Max, subs[l])
+                l+=1
+            bmax += Max
+    return bmax
+
+# cases = 1
+cases = int(input())
+for i in range(cases):
+    print(solution())
