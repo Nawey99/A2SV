e499719794e5f6968f1867d35d17d7f080bf4569
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-02-18 11:09:24 +0300

    Created 'Books' by A2SV Hub

---
+++ 18-Feb-2025/Books 270514.py
@@ -0,0 +1,17 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n,t = map(int,input().split())
+books = list(map(int,input().split()))
+res = 0
+r= 0
+l = 0
+Max = 0
+while r<n and l<=r:
+    res += books[r]
+    if res > t:
+        res -= books[l]
+        l += 1
+    Max = max(Max,r-l+1)
+    r += 1
+print(Max)
+    
