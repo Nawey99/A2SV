788774bee3077e39e56b54f70373e12e8f53f2f6
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-02-20 12:24:04 +0300

    Created 'Binary Subarrays With Sum' by A2SV Hub

---
+++ 20-Feb-2025/Binary Subarrays With Sum 276559.py
@@ -0,0 +1,21 @@
+# Problem: Binary Subarrays With Sum - https://leetcode.com/problems/binary-subarrays-with-sum/
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def helper(x):
+            if x<0: return 0
+
+            re = 0
+            l, cur = 0,0
+            for r in range(len(nums)):
+                cur += nums[r]
+
+                while cur>x:
+                    cur -= nums[l]
+                    l+=1
+                re+=(r-l+1)
+            return re
+
+
+        x= helper(goal) - helper(goal-1) 
+        return x
