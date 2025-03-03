6a298566422511685a56a4378c0ea448ab03412f
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-03-03 23:28:51 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 03-Mar-2025/B - Anansi and Trip-Photographs 290223.py
@@ -0,0 +1,17 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+
+def solution():
+    n,x = map(int,input().split())
+    num = list(map(int,input().split()))
+    num.sort()
+    for i in range (n):
+        y = num[i+n] - num[i]
+        if y < x:
+            return "NO"
+    return 'YES'
+
+# cases = 1
+cases = int(input())
+for i in range(cases):
+    print(solution())
