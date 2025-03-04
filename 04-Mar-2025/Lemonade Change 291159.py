625e24bdc84b2cd99fea0fe697eba0122cf3f982
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-03-04 16:52:27 +0300

    Created 'Lemonade Change' by A2SV Hub

---
+++ 04-Mar-2025/Lemonade Change 291159.py
@@ -0,0 +1,19 @@
+# Problem: Lemonade Change - https://leetcode.com/problems/lemonade-change/
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        count5=0
+        count10=0
+        for i in bills:
+            if i == 5:
+                count5+=1
+            elif i ==10:
+                count5 -=1
+                count10+=1
+            else:
+                count5-=1
+                count10 -=1
+            check = count5 + (count10*2)
+            if check < 0 or count5 < 0:
+                return False
+        return True
