102d26eda0aaa89ce4b35a45e1b134cf8a8073b5
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-03-04 08:21:53 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 04-Mar-2025/A - A+ Student 290494.py
@@ -0,0 +1,25 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+
+def solution():
+    count = 0
+    ans = []
+    mark = list(map(int,input().split()))
+    Max = 0
+    for i in range(3):
+        Max = max(Max, mark[i])
+    for i in mark:
+        if i == Max:
+            count +=1
+    for i in mark:
+        if Max == i and count == 1:
+            ans.append(0)
+        else:
+            x = Max +1
+            ans.append(x-i)
+    return ans
+
+# cases = 1
+cases = int(input())
+for i in range(cases):
+    print(*solution())
