3d17617bc78d9271fe54319067c4fa4677d0352a
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-03-04 16:48:05 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 04-Mar-2025/A - Language Identifier 291146.py
@@ -0,0 +1,18 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+
+def solution():
+    dic = {0:'FILIPINO',1:'JAPANESE',2:'JAPANESE',3:'KOREAN'}
+    y = ["po", "masu", "desu", "mnida"]
+    
+    x = input().strip()
+    
+    for i in range(len(y)):
+        suffix = y[i]
+        if x[-len(suffix):] == suffix: 
+            return dic[i]
+
+# cases = 1
+cases = int(input())
+for i in range(cases):
+    print(solution())
