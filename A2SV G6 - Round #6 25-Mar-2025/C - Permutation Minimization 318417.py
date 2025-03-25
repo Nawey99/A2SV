795c182d47c46c7e8ef08be25a7b93a816a6e618
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-03-25 23:47:36 +0300

    Created 'C - Permutation Minimization' by A2SV Hub

---
+++ A2SV G6 - Round #6 25-Mar-2025/C - Permutation Minimization 318417.py
@@ -0,0 +1,29 @@
+# Problem: C - Permutation Minimization - https://codeforces.com/gym/594077/problem/C
+
+import sys
+from collections import Counter,defaultdict, deque
+from itertools import accumulate 
+def I(): return int(sys.stdin.readline().strip())
+def II(): return map(int, sys.stdin.readline().strip().split())
+def IL(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def IS(): return sys.stdin.readline().strip().split()
+def psum(a): return [0] + list(accumulate(a))
+def solution():
+    n = I()
+    arr = IL()
+    que = deque()
+    n = len(arr)
+    que.append(arr[0])
+    for i in range(1,n):
+        if arr[i] <que[0]:
+            que.appendleft(arr[i])
+        else:
+            que.append(arr[i])
+        # print(que[0])
+    return que
+
+# cases = 1
+cases = I()
+for i in range(cases):
+    print(*solution())
