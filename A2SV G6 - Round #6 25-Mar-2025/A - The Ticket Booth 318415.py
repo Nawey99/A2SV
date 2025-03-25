dfd1f1ab4ed4654139a81e8694ea3c0403ab1bd1
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-03-25 23:47:23 +0300

    Created 'A - The Ticket Booth' by A2SV Hub

---
+++ A2SV G6 - Round #6 25-Mar-2025/A - The Ticket Booth 318415.py
@@ -0,0 +1,22 @@
+# Problem: A - The Ticket Booth - https://codeforces.com/gym/594077/problem/A
+
+import sys
+from collections import Counter,defaultdict
+from itertools import accumulate 
+def I(): return int(sys.stdin.readline().strip())
+def II(): return map(int, sys.stdin.readline().strip().split())
+def IL(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def IS(): return sys.stdin.readline().strip().split()
+def psum(a): return [0] + list(accumulate(a))
+def solution():
+    n,s = II()
+    x  = s//n
+    if s %n == 0:
+        return x
+    return x + 1
+
+cases = 1
+# cases = I()
+for i in range(cases):
+    print(solution())
