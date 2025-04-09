ac8a87ecbb324eb0a17d8821bb0af22456911176
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-04-09 17:13:28 +0300

    Created 'A - Escaping Prison' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 09-Apr-2025/A - Escaping Prison 329669.py
@@ -0,0 +1,22 @@
+# Problem: A - Escaping Prison - https://codeforces.com/gym/601269/problem/A
+
+import sys
+from collections import Counter,defaultdict
+from itertools import accumulate 
+def I(): return int(sys.stdin.readline().strip())
+def II(): return map(int, sys.stdin.readline().strip().split())
+def IL(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def IS(): return sys.stdin.readline().strip().split()
+def psum(a): return [0] + list(accumulate(a))
+def solution():
+    n,k = II()
+    Max = 0
+    for i in range(n):
+        Max += max(II())
+    return "YES" if Max >= k else "NO"
+
+# cases = 1
+cases = I()
+for i in range(cases):
+    print(solution())
