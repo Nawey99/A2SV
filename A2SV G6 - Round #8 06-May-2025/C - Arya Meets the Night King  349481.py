155440394884d650656a4452715d05c82f00419e
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-05-06 19:58:35 +0300

    Created 'C - Arya Meets the Night King ' by A2SV Hub

---
+++ A2SV G6 - Round #8 06-May-2025/C - Arya Meets the Night King  349481.py
@@ -0,0 +1,28 @@
+# Problem: C - Arya Meets the Night King  - https://codeforces.com/gym/599383/problem/C
+
+from bisect import bisect_right
+import sys
+from collections import Counter,defaultdict
+from itertools import accumulate 
+def I(): return int(sys.stdin.readline().strip())
+def II(): return map(int, sys.stdin.readline().strip().split())
+def IL(): return list(map(int, sys.stdin.readline().strip().split()))
+def S(): return sys.stdin.readline().strip()
+def IS(): return sys.stdin.readline().strip().split()
+def psum(a): return [0] + list(accumulate(a))
+s,b = II()
+attack =IL()
+arr =[]
+for _ in range(b):
+    arr.append(IL())
+arr.sort()
+power = [arr[i][0] for i in range(b)]
+defence = list(accumulate(arr[i][1] for i in range(b)))
+ans = []
+for atk in attack:
+    idx = bisect_right(power, atk) - 1
+    if idx >= 0:
+        ans.append(defence[idx])
+    else:
+        ans.append(0)
+print(*ans)
