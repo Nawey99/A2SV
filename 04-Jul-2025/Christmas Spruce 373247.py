1c3997a3a6d94c538be215018ce67c7f368a3cfd
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-07-04 12:06:06 +0300

    Created 'Christmas Spruce' by A2SV Hub

---
+++ 04-Jul-2025/Christmas Spruce 373247.py
@@ -0,0 +1,27 @@
+# Problem: Christmas Spruce - https://codeforces.com/contest/913/problem/B
+
+
+import sys
+from collections import defaultdict
+def I(): return int(sys.stdin.readline().strip())
+def solution():
+    children = defaultdict(list)
+    n = I()
+    for child in range(2,n+1):
+        parent = I()
+        children[parent].append(child)
+        
+    for node in range(1, n + 1):
+        if len(children[node]) > 0:  # internal node
+            leaf_count = 0
+            for child in children[node]:
+                if len(children[child]) == 0:
+                    leaf_count += 1
+            if leaf_count < 3:
+                return "No"
+    
+    return "Yes"
+
+cases = 1
+for i in range(cases):
+    print(solution())
