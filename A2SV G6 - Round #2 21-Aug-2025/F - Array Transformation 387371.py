4d00c58fabddb75a7c571d6fdd9fa41713e97630
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-08-21 04:35:56 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 21-Aug-2025/F - Array Transformation 387371.py
@@ -0,0 +1,34 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+import sys
+def I(): return int(sys.stdin.readline().strip())
+def II(): return map(int, sys.stdin.readline().strip().split())
+def IL(): return list(map(int, sys.stdin.readline().strip().split()))
+def solution():
+    n=I()
+    arr = IL()
+    count = Counter(arr)
+    
+    options = set(count.values())
+    min_deletions = float('inf')
+    
+    #c is always in range 1 to n
+    for c in range(1, n + 1):
+        if c not in options:
+            continue
+
+        deletions = 0
+        for num in count:
+            if count[num] < c:
+                deletions += count[num]
+            else:
+                deletions += count[num] - c
+        
+        min_deletions = min(min_deletions, deletions)
+    return min_deletions
+
+# cases = 1
+cases = I()
+for i in range(cases):
+    print(solution())
