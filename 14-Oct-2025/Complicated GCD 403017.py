5e6df0b1f42ed806b4b40557a3dadfb05d8e30eb
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-10-14 19:05:33 +0300

    Created 'Complicated GCD' by A2SV Hub

---
+++ 14-Oct-2025/Complicated GCD 403017.py
@@ -0,0 +1,19 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+import random
+import sys
+from collections import Counter,defaultdict
+from itertools import accumulate 
+def I(): return int(sys.stdin.readline().strip())
+def II(): return map(int, sys.stdin.readline().strip().split())
+def IL(): return list(map(int, sys.stdin.readline().strip().split()))
+def solution():
+    a,b = II()
+    if a==b:
+        return a
+    return 1
+
+cases = 1
+# cases = I()
+for i in range(cases):
+    print(solution())
