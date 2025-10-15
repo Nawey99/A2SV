229597683506487ca380bc01b4c66c02c4be0fa7
Nawey99 <85255142+Nawey99@users.noreply.github.com>
2025-10-15 17:06:57 +0300

    Created 'Row GCD' by A2SV Hub

---
+++ 15-Oct-2025/Row GCD 403341.py
@@ -0,0 +1,28 @@
+# Problem: Row GCD - https://codeforces.com/problemset/problem/1458/a
+
+import sys
+from math import gcd
+from collections import Counter,defaultdict
+from itertools import accumulate 
+def I(): return int(sys.stdin.readline().strip())
+def II(): return map(int, sys.stdin.readline().strip().split())
+def IL(): return list(map(int, sys.stdin.readline().strip().split()))
+def solution():
+    n,m = II()
+    arr = IL()
+    arr1 = IL()
+    store = []
+    arr.sort()
+    for i in range(1,n):
+        store.append(arr[i]-arr[i-1])
+
+    ans=[]
+    gcf = gcd(*store)
+    for i in arr1:
+        ans.append(gcd(gcf,i+arr[0]))
+    return ans
+        
+
+cases = 1
+for i in range(cases):
+    print(*solution())
